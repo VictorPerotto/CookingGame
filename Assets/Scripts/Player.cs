f9d4e5964a33617ed10c7777cad0b72674eff236
Victor Perotto Pioner <88547011+VictorPerotto@users.noreply.github.com>
2024-07-11 17:52:33 -0300

    Player Animation & Cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
+
     [SerializeField] private float movementSpeed;
+    private bool isWalking;
 
     private void Update(){
         Vector2 inputVector = new Vector2(0, 0);
@@ -26,7 +28,13 @@ public class Player : MonoBehaviour{
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * movementSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking(){
+        return isWalking;
     }
 }
