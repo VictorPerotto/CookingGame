a9749fa7db9e75b2fa9ce2c35bc29e1d7567173e
VictorPerotto <viperottopioner@gmail.com>
2024-07-11 23:42:55 -0300

    Refactored Input System & Player Physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
 
+    [SerializeField] private InputManager inputManager;
     [SerializeField] private float movementSpeed;
     private bool isWalking;
 
-    private void Update(){
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if(Input.GetKey(KeyCode.W)){
-            inputVector.y = 1;
-        }
-        if(Input.GetKey(KeyCode.S)){
-            inputVector.y = -1;
-        }
-        if(Input.GetKey(KeyCode.A)){
-            inputVector.x = -1;
-        }
-        if(Input.GetKey(KeyCode.D)){
-            inputVector.x = 1;
-        }
-
-        inputVector = inputVector.normalized;
+    private void Update(){    
+        Vector2 inputVector = inputManager.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * movementSpeed * Time.deltaTime;
 
+        float moveDistance = movementSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove){
+            Vector3 moveDirX = new Vector3 (moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove){
+                moveDir = moveDirX;
+            } else {
+                Vector3 moveDirZ = new Vector3 (0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if(canMove){
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+
+        if(canMove){
+            transform.position += moveDir * movementSpeed * Time.deltaTime;
+        }
+        
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
